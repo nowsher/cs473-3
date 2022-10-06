d4a25a9f329274cabce5c7aa3153c28e3a12c56a
nowsher <alinowsher@yahoo.com>
2022-10-05 23:28:12 -0500

    v2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.calculator)//.shop_categories //create_account//activity_main
 
         //////////////////////////////
 //        textFoodName.text = foodList.random()
